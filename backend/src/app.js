593ee880ee1bea887228f85513a55666fcd9f748
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-11-11 18:55:27 +0530

    connecting Imagekit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,23 @@
+// create server
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+const foodRoutes = require('./routes/foodRoute')
+const cors = require('cors');
 
 const app = express();
-
-// Middleware to parse JSON bodies
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+app.use(cookieParser());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server started successfully!');
-});
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
 
+app.use('/api/auth', authRoutes);
+app.use('/api/food', foodRoute);
 
 module.exports = app;
--- backend/src/routes/foodRoute.js
+++ backend/src/routes/foodRoute.js
@@ -1,7 +1,44 @@
-const express = require("express");
+const express = require('express');
 const foodController = require("../controllers/foodController")
+const authMiddleware = require("../middleware/authMiddleware")
 const router = express.Router();
+const multer = require('multer');
 
 
-router.post('/')
-module.exports = router;
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
+
+
+/* POST /api/food/ [protected]*/
+router.post('/',
+    authMiddleware.authFoodPartnerMiddleware,
+    upload.single("mama"),
+    foodController.createFood)
+
+
+/* GET /api/food/ [protected] */
+router.get("/",
+    authMiddleware.authUserMiddleware,
+    foodController.getFoodItems)
+
+
+router.post('/like',
+    authMiddleware.authUserMiddleware,
+    foodController.likeFood)
+
+
+router.post('/save',
+    authMiddleware.authUserMiddleware,
+    foodController.saveFood
+)
+
+
+router.get('/save',
+    authMiddleware.authUserMiddleware,
+    foodController.getSaveFood
+)
+
+
+
+module.exports = router
