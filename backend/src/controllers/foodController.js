cf0ccbcd37cc33f80390d0e3539472f5604b8f55
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-11-11 06:42:13 +0530

    Created authmiddleware and foodcontroller

---
+++ backend/src/controllers/foodController.js
@@ -0,0 +1,18 @@
+const foodModel = require('../models/foodModel');
+
+async function createFood(req, res) {
+    const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid())
+
+    const foodItem = await foodModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        video: fileUploadResult.url,
+        foodPartner: req.foodPartner._id
+    })
+
+    res.status(201).json({
+        message: "food created successfully",
+        food: foodItem
+    })
+
+}
---
+++ backend/src/routes/foodRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const foodController = require("../controllers/foodController")
+const router = express.Router();
+
+
+router.post('/')
+module.exports = router;
