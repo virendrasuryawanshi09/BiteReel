064edba4e1db15403bd6722ff0c9779137cb7ca8
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-11-11 06:23:46 +0530

    Created usermodel

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
+// start server
+require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 
 connectDB();
+
 app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
+    console.log("Server is running on port 5000");
+})
---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const authController = require("../controllers/authController")
+
+const router = express.Router();
+
+// user auth APIs
+router.post('/user/register', authController.registerUser)
+router.post('/user/login', authController.loginUser)
+router.get('/user/logout', authController.logoutUser)
